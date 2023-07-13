57ab2fffc63681f0deeeb63c9bb6dd34f1720542
Amrit Bhogal <frityet@node-1w7jr9pqz5biugcu5t3a9uw3r.ipv6.telus.net>
2023-07-13 16:26:54 -0700

    Working curlpp

--- packages.lua
+++ packages.lua
@@ -6,7 +6,7 @@ do
     add_versions("0.8.1", "67bb923bee565d1076baa6a758d299594ff0d8fd26fc5e02b83c5f5b5764ccee")
 
     on_install(function (package)
-        import("package.tools.cmake").install(package, {"-DBUILD_SHARED_LIBS=OFF"})
+        import("package.tools.cmake").install(package, {"-DBUILD_SHARED_LIBS=OFF"}, { packagedeps = { "libcurl" } })
     end)
 end
 package_end()
