ad8bc376ae04f6d8180bf23f0baf2d7f9463c84d
Frityet <ambhogal01@gmail.com>
2023-07-12 23:14:03 -0700

    Fix compile commands

--- packages.lua
+++ packages.lua
@@ -1,5 +1,6 @@
 package("curl-pp")
 do
+    add_deps("cmake", "libcurl")
     set_urls("https://github.com/jpbarrette/curlpp/archive/refs/tags/v$(version).zip")
 
     add_versions("0.8.1", "67bb923bee565d1076baa6a758d299594ff0d8fd26fc5e02b83c5f5b5764ccee")
--- src/pages/AdditionalTools.cpp
+++ src/pages/AdditionalTools.cpp
@@ -1,3 +1,4 @@
+
 #include "AdditionalTools.hpp"
 #include "utilities.hpp"
 
@@ -5,13 +6,15 @@
 #include <QtWidgets/QCheckBox>
 using namespace pages;
 
+AdditionalTools::AdditionalTools(QWidget *parent) : QWizardPage(parent)
+{}
 
 void AdditionalTools::initializePage()
 {
     this->setTitle("Select Luarocks version");
     this->setSubTitle("Select the version of Luarocks you want to install.");
     auto layout = new QVBoxLayout(this);
-     auto _checkbox1 = new QCheckBox("Checkbox 1");
+    auto _checkbox1 = new QCheckBox("Checkbox 1");
     auto _checkbox2 = new QCheckBox("Checkbox 2");
     auto _checkbox3 = new QCheckBox("Checkbox 3");
     layout->addWidget(_checkbox1);
