93f7c05089c88a5b130410a63dd3ee9cb630464f
Amrit Bhogal <ambhogal01@gmail.com>
2023-07-14 13:10:42 -0700

    optional vcpkg

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,42 @@
-
-
 local packages = {
     "qt5widgets",
-    "vcpkg::curlpp",
 }
+
+option "vcpkg-curlpp" do
+    set_default(false)
+    set_showmenu(true)
+    set_category("option")
+    set_description("Use vcpkg curlpp")
+end
+option_end()
+
+if has_config("vcpkg-curlpp") then
+    packages[#packages+1] = "vcpkg::curlpp"
+else
+    package "curlpp-local" do
+        add_deps("cmake")
+        add_deps("libcurl")
+        set_urls("https://github.com/jpbarrette/curlpp/archive/refs/tags/v$(version).zip")
+
+        add_versions("0.8.1", "67bb923bee565d1076baa6a758d299594ff0d8fd26fc5e02b83c5f5b5764ccee")
+
+        on_install(function(package)
+            local lcurl = package:dep("libcurl")
+
+            import("package.tools.cmake").install(package, {
+                "-DCURLPP_BUILD_SHARED_LIBS="..(package:config("shared") and "ON" or "OFF"),
+                "-DBUILD_SHARED_LIBS="..(package:config("shared") and "ON" or "OFF"),
+                "-DCURLPP_STATIC_CRT=ON",
+                "-DCURL_INCLUDE_DIRS=" .. lcurl:installdir("include"),
+                "-DCURL_LIBRARIES=" .. lcurl:installdir("lib", "libcurl.a"),
+
+            }, { packagedeps = { "libcurl" } })
+        end)
+    end
+    package_end()
+
+    packages[#packages+1] = "curlpp-local"
+end
 
 local sanitizers = { "address", "leak", "undefined" }
 
@@ -21,11 +54,11 @@ local cxxflags = {
 
 
 if is_plat("windows") then
-    cxxflags.regular[#cxxflags.regular+1] = "-FS"
+    cxxflags.regular[#cxxflags.regular + 1] = "-FS"
 else
-    cxxflags.regular[#cxxflags.regular+1] = "-Wall"
-    cxxflags.regular[#cxxflags.regular+1] = "-Wextra"
-    cxxflags.regular[#cxxflags.regular+1] = "-Werror"
+    cxxflags.regular[#cxxflags.regular + 1] = "-Wall"
+    cxxflags.regular[#cxxflags.regular + 1] = "-Wextra"
+    cxxflags.regular[#cxxflags.regular + 1] = "-Werror"
 end
 
 local ldflags = {
@@ -38,11 +71,11 @@ set_languages("cxxlatest")
 
 add_rules("mode.debug", "mode.release")
 
-add_requires(packages)
+add_requires(packages, { system = false, configs = { shared = true } })
 
-target("LuaInstaller")
-do
+target "LuaInstaller" do
     add_rules("qt.widgetapp")
+    add_frameworks("QtGui", "QtCore")
     set_kind("binary")
     add_packages(packages)
     add_files("src/**.cpp")
@@ -68,4 +101,3 @@ do
         add_ldflags(ldflags.release)
     end
 end
-target_end()
